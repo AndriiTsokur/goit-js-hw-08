1010e005af1696ee07d7e865380585f28478bf80
Andrii Tsokur <tsokur@gmail.com>
2023-04-22 00:55:27 +0300

    Adds minor improves to Task 1 JS structure

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,18 +1,9 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import makeGalleryMarkup from './01/makeGalleryMarkup';
+import launchSimpleLightbox from './01/launchSimpleLightbox';
 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 makeGalleryMarkup(galleryItems);
-
-const lightbox = new SimpleLightbox('.gallery a', {
-	overlayOpacity: 0.85,
-	captionsData: 'alt',
-	captionDelay: 250,
-});
-
-lightbox.on('show.simplelightbox');
+launchSimpleLightbox();
