108b9ed1c91e87f16d2d3e952e1c704b5a72ff67
Andrii Tsokur <tsokur@gmail.com>
2023-04-22 00:58:33 +0300

    Adds minor improvements to Task 1 JS structure

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@ import makeGalleryMarkup from './01/makeGalleryMarkup';
 import launchSimpleLightbox from './01/launchSimpleLightbox';
 
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './01/gallery-items';
 // Change code below this line
 
 makeGalleryMarkup(galleryItems);
